315da8fee2942adf8a79d924e77cebcd2411b7cd
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-11 23:34:32 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,15 @@
+'''
+DATE:11TH NOV 2022
+DAY: SUNDAY
+TOPIC: FOR LOOP
+AUTHOR: RAMA BHARGAVI
+'''
+# a= 1,2,3
+# print(*a) #1 2 3
+# for i  in a:
+#     print(i,end=' ') #1 2 3
+# for i in range(5):
+    # print(i) # 1 2 3 4 (vertically)   
+n=int(input('enter the value of n:'))
+for i in range(1,n+1):
+    print(i,end=" ")    # 1 2 3 4 5
